d340ef695d6536da217b7dab509897a717cc7b96
PMA213X <PMA213X@noreply.gitcode.com>
2024-12-24 16:12:58 +0800

    1

--- live_info.py
+++ live_info.py
@@ -1,30 +1,32 @@
 import requests
 import re
 #直播间基本信息提取
-url = 'https://api.live.bilibili.com/room/v1/Room/get_info'
-params = {
-        'room_id':31368697
+def livedata(room_id):
+
+    url = 'https://api.live.bilibili.com/room/v1/Room/get_info'
+    params = {
+            'room_id':room_id
+        }
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'application/xml',
+        'Referer': 'https://www.bilibili.com/',
+        'Origin': 'https://www.bilibili.com',
+        'Cookie': 'your_cookie_here',  
     }
-headers = {
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Accept': 'application/xml',
-    'Referer': 'https://www.bilibili.com/',
-    'Origin': 'https://www.bilibili.com',
-    'Cookie': 'your_cookie_here',  
-}
 
-response = requests.get(url=url, headers=headers, params=params)
+    response = requests.get(url=url, headers=headers, params=params)
 
-response.encoding=response.apparent_encoding
-data = response.json()
-room_data = data['data']
-print(f"UID: {room_data['uid']}")
-print(f"Room ID: {room_data['room_id']}")
-print(f"Attention: {room_data['attention']}")
-print(f"Online: {room_data['online']}")
-print(f"Live Status: {'直播中' if room_data['live_status'] == 1 else '未直播'}")
-print(f"Title: {room_data['title']}")
-print(f"Live Time: {room_data['live_time']}")
-print(f"Area Name: {room_data['area_name']}")
-print(f"Parent Area Name: {room_data['parent_area_name']}")
-print("Hot Words:", ", ".join(room_data['hot_words']))
+    response.encoding=response.apparent_encoding
+    data = response.json()
+    room_data = data['data']
+    print(f"UID: {room_data['uid']}")
+    print(f"Room ID: {room_data['room_id']}")
+    print(f"Attention: {room_data['attention']}")
+    print(f"Online: {room_data['online']}")
+    print(f"Live Status: {'直播中' if room_data['live_status'] == 1 else '未直播'}")
+    print(f"Title: {room_data['title']}")
+    print(f"Live Time: {room_data['live_time']}")
+    print(f"Area Name: {room_data['area_name']}")
+    print(f"Parent Area Name: {room_data['parent_area_name']}")
+    print("Hot Words:", ", ".join(room_data['hot_words']))
--- ship.py
+++ ship.py
@@ -4,85 +4,85 @@ import re
 import json
 import user
 
+def shipdata(nameid,room_id,ruid):
+    # 直播间基本信息提取
+    base_url = 'https://api.live.bilibili.com/xlive/app-room/v2/guardTab/topList'
+    room_id = room_id
+    ruid = ruid
+    page_size = 27
+    a = nameid
+    b = f"{a}{int(time())}"
+    ship = []
 
-# 直播间基本信息提取
-base_url = 'https://api.live.bilibili.com/xlive/app-room/v2/guardTab/topList'
-room_id = 21672023
-ruid = 477317922
-page_size = 27
-a = 'Miki'
-b = f"{a}{int(time())}"
-ship = []
+    def save_to_json(data, filename):
+        with open(filename + '.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
 
-def save_to_json(data, filename):
-    with open(filename + '.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Accept': 'application/json',
-    'Referer': 'https://www.bilibili.com/',
-    'Origin': 'https://www.bilibili.com',
-    'Cookie': 'your_cookie_here',
-}
-
-def print_user_info(item, prefix=""):
-    rank = item.get('rank', 'N/A')
-    username = item.get('username', 'N/A')
-    medal_name = item['medal_info'].get('medal_name', 'N/A') if 'medal_info' in item else 'N/A'
-    medal_level = item['medal_info'].get('medal_level', 'N/A') if 'medal_info' in item else 'N/A'
-    accompany = item.get('accompany', 'N/A')
-    uid = item.get('uid', 'N/A')
-    
-    # 获取用户更多信息
-    user_info = user.get_user_info(uid)
-    gender = user_info['性别']
-    level = user_info['等级']
-    vip_type = user_info['VIP类型']
-    official_type = user_info['官方认证类型']
-    
-    print(f"{prefix}排名: {rank},\t  粉丝牌名称: {medal_name},    "
-          f"粉丝牌等级: {medal_level},    "
-          f"\t性别: {gender},\t等级: {level},"
-          f"\tVIP类型: {vip_type},\t官方认证类型: {official_type},\t大航海陪伴时间: {accompany},\t用户名: {username}")
-    
-    user_info_dict = {
-        'rank': rank,
-        'username': username,
-        'medal_name': medal_name,
-        'medal_level': medal_level,
-        'accompany': accompany,
-        'uid': uid,
-        '性别': gender,
-        '等级': level,
-        'VIP类型': vip_type,
-        '官方认证类型': official_type
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'application/json',
+        'Referer': 'https://www.bilibili.com/',
+        'Origin': 'https://www.bilibili.com',
+        'Cookie': 'your_cookie_here',
     }
-    
-    # 添加到 ship 列表
-    ship.append(user_info_dict)
 
-page = 1
+    def print_user_info(item, prefix=""):
+        rank = item.get('rank', 'N/A')
+        username = item.get('username', 'N/A')
+        medal_name = item['medal_info'].get('medal_name', 'N/A') if 'medal_info' in item else 'N/A'
+        medal_level = item['medal_info'].get('medal_level', 'N/A') if 'medal_info' in item else 'N/A'
+        accompany = item.get('accompany', 'N/A')
+        uid = item.get('uid', 'N/A')
+        
+        # 获取用户更多信息
+        user_info = user.get_user_info(uid)
+        gender = user_info['性别']
+        level = user_info['等级']
+        vip_type = user_info['VIP类型']
+        official_type = user_info['官方认证类型']
+        
+        print(f"{prefix}排名: {rank},\t  粉丝牌名称: {medal_name},    "
+            f"粉丝牌等级: {medal_level},    "
+            f"\t性别: {gender},\t等级: {level},"
+            f"\tVIP类型: {vip_type},\t官方认证类型: {official_type},\t大航海陪伴时间: {accompany},\t用户名: {username}")
+        
+        user_info_dict = {
+            'rank': rank,
+            'username': username,
+            'medal_name': medal_name,
+            'medal_level': medal_level,
+            'accompany': accompany,
+            'uid': uid,
+            '性别': gender,
+            '等级': level,
+            'VIP类型': vip_type,
+            '官方认证类型': official_type
+        }
+        
+        # 添加到 ship 列表
+        ship.append(user_info_dict)
 
-while True:
-    url = f'{base_url}?roomid={room_id}&page={page}&ruid={ruid}&page_size={page_size}'
-    response = requests.get(url=url, headers=headers)
-    data = response.json()
-    
-    user_list = data.get('data', {}).get('list', [])
-    top3_list = data.get('data', {}).get('top3', [])
-    if page == 1:
-        for item in top3_list:
-            print_user_info(item, prefix="Top ")
-    
-    if not user_list:
-        break
-    
-    # 提取并打印 'list' 中的用户信息
-    for item in user_list:
-        print_user_info(item)
-    
-    page += 1
+    page = 1
 
-# 保存到本地文件夹
-save_to_json(ship, b)
+    while True:
+        url = f'{base_url}?roomid={room_id}&page={page}&ruid={ruid}&page_size={page_size}'
+        response = requests.get(url=url, headers=headers)
+        data = response.json()
+        
+        user_list = data.get('data', {}).get('list', [])
+        top3_list = data.get('data', {}).get('top3', [])
+        if page == 1:
+            for item in top3_list:
+                print_user_info(item, prefix="Top ")
+        
+        if not user_list:
+            break
+        
+        # 提取并打印 'list' 中的用户信息
+        for item in user_list:
+            print_user_info(item)
+        
+        page += 1
+
+    # 保存到本地文件夹
+    save_to_json(ship, b)
