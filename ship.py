22ad53d215ad631eb67209d9d9da15a45fa1195e
PMA213X <PMA213X@noreply.gitcode.com>
2024-12-25 09:11:52 +0800

    1

--- ship.py
+++ ship.py
@@ -1,3 +1,4 @@
+import random
 from time import sleep, time
 import requests
 import re
@@ -39,6 +40,7 @@ def shipdata(room_id,ruid):
         uid = item.get('uid', 'N/A')
         
         # 获取用户更多信息
+        sleep(random.random() * 4)
         user_info = user.get_user_info(uid)
         gender = user_info['性别']
         level = user_info['等级']
